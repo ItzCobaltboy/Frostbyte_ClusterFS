09f4b96e061d568deda0133dc91b305ac6b70669
ItzCobaltboy <pathanaffan62@gmail.com>
2025-06-29 21:36:26 +0530

    added license in code

--- frostbyte-datanode/src/main/java/org/frostbyte/datanode/App.java
+++ frostbyte-datanode/src/main/java/org/frostbyte/datanode/App.java
@@ -1,11 +1,20 @@
 package org.frostbyte.datanode;
 
+/*
+ * Frostbyte - Encrypted Distributed Object Storage
+ * Copyright (c) 2025 Affan Pathan
+ *
+ * Licensed for non-commercial use only.
+ * Commercial usage requires a separate license.
+ * Contact: your-email@example.com
+ */
+
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.frostbyte.datanode.models.configModel;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
